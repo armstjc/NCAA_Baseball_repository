fb21c104946bcf0f6d2ebed080b160c99833fd3e
Joseph Armstrong <armstjc@mail.uc.edu>
2022-08-23 11:02:29 -0400

    08/23/2022 Stats Update

--- getCbbGameStats.py
+++ getCbbGameStats.py
@@ -44,7 +44,7 @@ def getAllGbgStats():
     #print(schools)
     hasRoster = True
 
-    for s in range(169,len(schools)):
+    for s in range(180,len(schools)):
         i = schools[s]
         print(i)
         try:

e0b2fd7d483474e4f9532978eaa6a21d47c55d44
Joseph Armstrong <armstjc@mail.uc.edu>
2022-08-29 13:19:28 -0400

    Began the download of fielding data

--- getCbbGameStats.py
+++ getCbbGameStats.py
@@ -11,7 +11,7 @@ import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
 def getSchoolList():
-    schools = datasets.get_school_table()
+    schools = datasets.get_school_path()
     school_df = pd.read_parquet(schools)
     #print(school_df)
     school_arr = school_df['ncaa_name'].to_numpy()
@@ -80,6 +80,7 @@ def getAllGbgStats():
                     player_id = 0
                 print(f'{count}/{maxRost} {season_id} {player_name}')
                 data_b = ncaa.ncaa_player_game_logs(school=school_name, player=player_name, season=season_id, variant='batting')
+                data_b = data_b[data_b['AB'] != 0]
                 try:
                     if (len(data_b)==0):
                     #print('Nothing to save')
@@ -91,6 +92,7 @@ def getAllGbgStats():
                 #print(data_b)
 
                 data_p = ncaa.ncaa_player_game_logs(school=school_name, player=player_name, season=season_id, variant='pitching')
+                data_p = data_p.loc[data_p['App']>0]
                 try:
                     data_p = data_p[data_p['OrdAppeared'] != 0]
                     if (len(data_p)==0):
@@ -139,80 +141,27 @@ def getSeasonGbgStats(season=2020):
                 player_name = rost['name'][j]
                 player_id = rost['stats_player_seq'][j]
                 print(f'{count}/{maxRost} {school_name} {season} {player_name}')
-                data_b = ncaa.ncaa_player_game_logs(school=school_name, player=player_name, season=season_id, variant='batting')
+                data_b = ncaa.ncaa_player_game_logs(player=player_name, season=season_id, variant='batting',  school=school_name)
+                
+                data_b = data_b[data_b['AB'] != 0]
                 
                 if len(data_b) > 0:
                     data_b.to_csv(f'PlayerStats/Batting/{season_id}_{player_id}.csv',index=False)
             
-                data_p = ncaa.ncaa_player_game_logs(school=school_name, player=player_name, season=season_id, variant='pitching')
-
-                data_p.to_csv(f'PlayerStats/Pitching/{season_id}_{player_id}.csv',index=False)
+                data_p = ncaa.ncaa_player_game_logs( player=player_name,season=season_id, variant='pitching',  school=school_name)
                 
-                time.sleep(5)
+                data_p = data_p.loc[data_p['App']>0]
+                #print(data_p)
+                if len(data_p) > 0:
+                    data_p.to_csv(f'PlayerStats/Pitching/{season_id}_{player_id}.csv',index=False)
 
-def mergeFiles(filePath=""):
-    main_df = pd.DataFrame()
-    f = 0
-    l = filePath
+                time.sleep(4)
 
-    for file in glob.iglob(l+"/*csv"):
-        f +=1
-    for file in tqdm(glob.iglob(l+"/*csv"),total=f,ascii=True, bar_format='{l_bar}{bar:30}{r_bar}{bar:-30b}'):
-        try:
-            df = pd.read_csv(file)
-            main_df = pd.concat([main_df,df],ignore_index=True)
-        except:
-            pass
-    return main_df
-
-def mergeBattingLogs():
-    f = "PlayerStats/Batting"
-    df = mergeFiles(f)
-    df.to_csv("PlayerStats/batting_logs.csv",index=False)
-
-def mergePitchingLogs():
-    f = "PlayerStats/Pitching"
-    df = mergeFiles(f)
-    df.to_csv("PlayerStats/pitching_logs.csv",index=False)
-
-def splitBattingStats():
-    print('Reading the batting logs file.')
-    df = pd.read_csv('PlayerStats/batting_logs.csv')
-    print('Done!\n')
-    max_season = df['season'].max()
-    min_season = df['season'].min()
-    for i in range(min_season,max_season+2):
-        print(f'Creating batting logs for the {i} season.')
-        s_df = df[df['season'] == i]
-        partOne = s_df.sample(frac=0.5)
-        partTwo = s_df.drop(partOne.index)
-
-        partOne.to_csv(f'PlayerStats/{i}_01_batting.csv',index=False)
-        partTwo.to_csv(f'PlayerStats/{i}_02_batting.csv',index=False)
-        #s_df.to_csv(f'PlayerStats/{i}_batting.csv')
-
-def splitPitchingStats():
-    print('Reading the pitching logs file.')
-    df = pd.read_csv('PlayerStats/pitching_logs.csv')
-    print('Done!\n')
-    max_season = df['season'].max()
-    min_season = df['season'].min()
-    for i in range(min_season,max_season+1):
-        print(f'Creating pitching logs for the {i} season.')
-        s_df = df[df['season'] == i]
-        partOne = s_df.sample(frac=0.5)
-        partTwo = s_df.drop(partOne.index)
-
-        partOne.to_csv(f'PlayerStats/{i}_01_pitching.csv',index=False)
-        partTwo.to_csv(f'PlayerStats/{i}_02_pitching.csv',index=False)
-        #s_df.to_csv(f'PlayerStats/{i}_batting.csv')
 
 def main():
     print('starting up')
-    mergeBattingLogs()
-    mergePitchingLogs()
-    #getAllGbgStats()
+    getSeasonGbgStats(season=2015)
 
     
 if __name__ == "__main__":
-    main()
+    main() 

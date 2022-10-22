65f2ad8a6b2306e13db14960509d90e91fac7a75
Joseph Armstrong <armstjc@mail.uc.edu>
2022-10-22 18:39:12 -0400

    10/22/2022 Stats Update 2
    - 702/945 teams (74.3%) have their stats completely downloaded.

--- getCbbGameStats.py
+++ getCbbGameStats.py
@@ -42,7 +42,7 @@ def getAllGbgStats():
     hasRoster = True
     school_count = 0
     schools_len = len(schools)
-    for s in range(661,len(schools)+1):
+    for s in range(702,len(schools)+1):
         #school_count += 1
         school_count = s
         i = schools[s]

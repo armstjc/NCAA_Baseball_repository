429d0a2bbfdae9d4a51814f78167b9862eba6ddd
armstjc <70788681+armstjc@users.noreply.github.com>
2023-02-24 18:36:51 -0500

    Merge pull request #9 from armstjc/armstjc-refactor-combine_py

--- combine.py
+++ combine.py
@@ -4,7 +4,6 @@ from tqdm import tqdm
 import pandas as pd
 import numpy as np
 from multiprocessing import Pool
-from datetime import datetime
 
 def reader(filename):
         #print(f'Reading in {filename}')
@@ -33,31 +32,12 @@ def mergeFilesMultithreaded(filePath=""):
 
     return main_df
 
-def load_ncaa_roster():
-    print('Loading in NCAA Rosters')
-
-    main_df = pd.DataFrame()
-    season_df = pd.DataFrame()
-    current_year = datetime.now().year
-    for i in range(2013,current_year+1):
-        print(f'Loading in the {i} roster.')
-        try:
-            season_df = pd.read_csv(f'TeamRosters/{i}_roster.csv')
-            main_df = pd.concat([main_df,season_df])
-        except:
-            print(f'Could not load in the roster file for the {i} season. The file may not exist, or it may be in use.')
-
-    main_df = main_df.dropna(subset=['player_id'])
-    print('Finished loading and combining rosters.')
-    return main_df
-
 def merge_batting_stats():
     print('Starting the merge of Batting CSVs.')
     f = f"PlayerStats/batting"
     print('Collecting downloaded CSVs.')
     df = mergeFilesMultithreaded(os.path.abspath(f))
     print('Done!')
-    
     max_season = df['season'].max()
     min_season = df['season'].min()
     for i in range(min_season,max_season+1):
@@ -135,139 +115,11 @@ def merge_rosters():
         s_df = df[df['season'] == i]
         s_df.to_csv(f'TeamRosters/{i}_roster.csv',index=False)
 
-def add_divisions():
-    for i in tqdm(range(2013,2024)):
-        print(f'Adding NCAA Divisions to the {i} batting stats.')
-        df = pd.read_parquet(f'game_stats/player/batting_game_stats/parquet/{i}_batting.parquet')
-        try:
-            df = df.drop(columns=['division'])
-        except:
-            print('\n[division] not found in this season\'s batting stats.')
-
-        try:
-            df = df.filter(items=['R', 'AB', 'H', '2B', '3B', 'TB', 'HR', 'RBI', 'BB', 'HBP', 'SF', 'SH', 'K', 'DP', 'CS', 'Picked', 'SB', 'IBB', 'date', 'field', 'season_id', 'opponent_id', 'opponent_name', 'innings_played', 'extras', 'runs_scored', 'runs_allowed', 'run_difference', 'result', 'game_id', 'school_id', 'stats_player_seq', 'season', 'OPP DP', 'GDP','score'])
-        except:
-            print('Dataframe was ready for divisions to be added.')
-
-        roster_df = pd.read_csv(f"TeamRosters/{i}_roster.csv")
-        roster_df = roster_df.filter(items=['player_id','division'])
-        roster_df = roster_df.dropna()
-        roster_df = roster_df.drop_duplicates()
-        # players_dict = pd.Series(roster_df.player_id,index=roster_df.season_id).to_dict()
-        # df['division'] = df['stats_player_seq'].map(players_dict)
-        df = df.merge(roster_df, left_on='stats_player_seq', right_on='player_id')
-        #roster_df = roster_df.astype({'division':'int32'})
-        del roster_df
-
-        df.to_parquet(f'game_stats/player/batting_game_stats/parquet/{i}_batting.parquet')
-
-        len_df = len(df)
-        len_df = len_df // 4
-        partOne = df.iloc[:len_df]
-        partTwo = df.iloc[len_df:2*len_df]
-        partThree = df.iloc[2*len_df:3*len_df]
-        partFour = df.iloc[3*len_df:]
-
-        del df
-
-        partOne.to_csv(f'game_stats/player/batting_game_stats/csv/{i}_batting_01.csv',index=False)
-        partTwo.to_csv(f'game_stats/player/batting_game_stats/csv/{i}_batting_02.csv',index=False)
-        partThree.to_csv(f'game_stats/player/batting_game_stats/csv/{i}_batting_03.csv',index=False)
-        partFour.to_csv(f'game_stats/player/batting_game_stats/csv/{i}_batting_04.csv',index=False)
-
-        del partOne,partTwo,partThree,partFour
-
-    for i in tqdm(range(2013,2024)):
-        print(f'Adding NCAA Divisions to the {i} pitching stats.')
-        df = pd.read_parquet(f'game_stats/player/pitching_game_stats/parquet/{i}_pitching.parquet')
-        try:
-            df = df.drop(columns=['division'])
-        except:
-            print('\n[division] not found in this season\'s pitching stats.')
-
-        try:
-            df = df.filter(items=['App','GS','IP','CG','H','R','ER','BB','SO','SHO','BF','P-OAB','2B-A','3B-A','Bk','HR-A','WP','HB','IBB','Inh Run','Inh Run Score','SHA','SFA','pitches','GO','FO','W','L','SV','OrdAppeared','KL','date','field','season_id','opponent_id','opponent_name','innings_played','extras','runs_scored','runs_allowed','run_difference','result','game_id','school_id','stats_player_seq','season','pickoffs','score','Pitches','player_id'])
-        except:
-            print('Dataframe was ready for divisions to be added.')
-            
-        roster_df = pd.read_csv(f"TeamRosters/{i}_roster.csv")
-        roster_df = roster_df.filter(items=['player_id','division'])
-        roster_df = roster_df.dropna()
-        roster_df = roster_df.drop_duplicates()
-        # players_dict = pd.Series(roster_df.player_id,index=roster_df.season_id).to_dict()
-        # df['division'] = df['stats_player_seq'].map(players_dict)
-        df = df.merge(roster_df, left_on='stats_player_seq', right_on='player_id')
-        #roster_df = roster_df.astype({'division':'int32'})
-        del roster_df
-
-        df.to_parquet(f'game_stats/player/fielding_game_stats/parquet/{i}_pitching.parquet')
-
-        len_df = len(df)
-        len_df = len_df // 4
-        partOne = df.iloc[:len_df]
-        partTwo = df.iloc[len_df:2*len_df]
-        partThree = df.iloc[2*len_df:3*len_df]
-        partFour = df.iloc[3*len_df:]
-
-        del df
-
-        partOne.to_csv(f'game_stats/player/pitching_game_stats/csv/{i}_pitching_01.csv',index=False)
-        partTwo.to_csv(f'game_stats/player/pitching_game_stats/csv/{i}_pitching_02.csv',index=False)
-        partThree.to_csv(f'game_stats/player/pitching_game_stats/csv/{i}_pitching_03.csv',index=False)
-        partFour.to_csv(f'game_stats/player/pitching_game_stats/csv/{i}_pitching_04.csv',index=False)
-
-        del partOne,partTwo,partThree,partFour
-
-    for i in tqdm(range(2013,2024)):
-        print(f'Adding NCAA Divisions to the {i} fielding stats.')
-        df = pd.read_parquet(f'game_stats/player/fielding_game_stats/parquet/{i}_fielding.parquet')
-        try:
-            df = df.drop(columns=['division'])
-        except:
-            print('\n[division] not found in this season\'s fielding stats.')
-
-
-        try:
-            df = df.filter(items=['PO','A','TC','E','CI','PB','SBA','CSB','IDP','TP','date','field','season_id','opponent_id','opponent_name','innings_played','extras','runs_scored','runs_allowed','run_difference','result','game_id','school_id','stats_player_seq','season'])
-        except:
-            print('Dataframe was ready for divisions to be added.')
-
-        roster_df = pd.read_csv(f"TeamRosters/{i}_roster.csv")
-        roster_df = roster_df.filter(items=['player_id','division'])
-        roster_df = roster_df.dropna()
-        roster_df = roster_df.drop_duplicates()
-        # players_dict = pd.Series(roster_df.player_id,index=roster_df.season_id).to_dict()
-        # df['division'] = df['stats_player_seq'].map(players_dict)
-        df = df.merge(roster_df, left_on='stats_player_seq', right_on='player_id')
-        #roster_df = roster_df.astype({'division':'int32'})
-        del roster_df
-
-        df.to_parquet(f'game_stats/player/fielding_game_stats/parquet/{i}_fielding.parquet')
-
-        len_df = len(df)
-        len_df = len_df // 4
-        partOne = df.iloc[:len_df]
-        partTwo = df.iloc[len_df:2*len_df]
-        partThree = df.iloc[2*len_df:3*len_df]
-        partFour = df.iloc[3*len_df:]
-
-        del df
-
-        partOne.to_csv(f'game_stats/player/fielding_game_stats/csv/{i}_fielding_01.csv',index=False)
-        partTwo.to_csv(f'game_stats/player/fielding_game_stats/csv/{i}_fielding_02.csv',index=False)
-        partThree.to_csv(f'game_stats/player/fielding_game_stats/csv/{i}_fielding_03.csv',index=False)
-        partFour.to_csv(f'game_stats/player/fielding_game_stats/csv/{i}_fielding_04.csv',index=False)
-
-        del partOne,partTwo,partThree,partFour
-
-
-
 def main():
-    # merge_rosters()
-    # merge_batting_stats()
-    # merge_pitching_stats()
-    # merge_fielding_stats()
-    add_divisions()
+    merge_rosters()
+    merge_batting_stats()
+    merge_pitching_stats()
+    merge_fielding_stats()
 
 if __name__ == "__main__":
     main()

e6dd27cf9789caa14dc5b55f148238073e1874f7
Joseph Armstrong <armstjc@mail.uc.edu>
2023-04-15 19:59:35 -0400

    4/15/2023 Stats Update

--- get_ncaa_baseball_stats.py
+++ get_ncaa_baseball_stats.py
@@ -118,7 +118,8 @@ def get_season_ncaa_baseball_stats(season:int,division:int):
         time.sleep(4)
 
 def main():
-    get_season_ncaa_baseball_stats(2023,0)
+    get_season_ncaa_baseball_stats(2023,2)
+    get_season_ncaa_baseball_stats(2023,3)
 
 if __name__ == "__main__":
     main()
